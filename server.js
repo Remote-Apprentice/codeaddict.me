22090a820af3547bc52ff229a82b8598dbdad8c1
javajohnhub <joedw1978@gmail.com>
2017-05-28 10:52:18 -0400

    express middleware

--- server.js
+++ server.js
@@ -7,11 +7,13 @@ var express = require('express')
     , Room = require('./room.js')
     , _ = require('underscore')._;
 var sanitize = require('validator');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
 
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
 app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
-app.use(express.bodyParser());
-app.use(express.methodOverride());
+app.use(bodyParser());
+app.use(methodOverride());
 app.use(express.static(__dirname + '/public'));
 app.use('/components', express.static(__dirname + '/components'));
 app.use('/js', express.static(__dirname + '/js'));
