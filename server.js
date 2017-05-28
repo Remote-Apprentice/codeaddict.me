50c54fdc43dd7191a350f79ebffd0ee5396808d9
javajohnhub <joedw1978@gmail.com>
2017-05-28 10:49:35 -0400

    update for express

--- server.js
+++ server.js
@@ -8,17 +8,16 @@ var express = require('express')
     , _ = require('underscore')._;
 var sanitize = require('validator');
 
-app.configure(function() {
-    app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 80);
-    app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "codeaddict.me");
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(express.static(__dirname + '/public'));
-    app.use('/components', express.static(__dirname + '/components'));
-    app.use('/js', express.static(__dirname + '/js'));
-    app.use('/icons', express.static(__dirname + '/icons'));
-    app.set('views', __dirname + '/views');
-    app.engine('html', require('ejs').renderFile);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
+app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.static(__dirname + '/public'));
+app.use('/components', express.static(__dirname + '/components'));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/icons', express.static(__dirname + '/icons'));
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
 
 	/* Store process-id (as priviledged user) */
     try {
@@ -27,8 +26,6 @@ app.configure(function() {
         console.log(err);
         //process.exit(1);
     }
-
-});
 
 app.get('/', function(req, res) {
     res.render('index.html');
