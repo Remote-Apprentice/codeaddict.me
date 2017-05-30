3945ba0781a720eb9384ad98d04c582aec143e9c
javajohnhub <joedw1978@gmail.com>
2017-05-29 21:39:27 -0400

    change port for heroic

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var sanitize = require('validator');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 
-app.set('port', 8080);
+//app.set('port', 8080);
 app.set('ipaddr', "codeaddict.me");
 app.use(bodyParser());
 app.use(methodOverride());
@@ -26,7 +26,7 @@ app.get('/', function(req, res) {
     res.render('index.html');
 });
 
-server.listen(app.get('port'), app.get('ipaddr'), function(){
+server.listen(port, app.get('ipaddr'), function(){
     console.log('Express server listening on  IP: ' + app.get('ipaddr') + ' and port ' + app.get('port'));
 });
 
